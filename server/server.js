13e880a4bd2251f2a7860b8615978b524834c3ba
Hugo Rodriguez <hi.rodriguez1998@gmail.com>
2021-03-25 16:48:51 -0300

    Added Home page and fixed dependencies version

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
 import config from './../config/config'
 import app from './express'
-app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err)
-    }
-    console.info('Server started on port %s.', config.port)
+import mongoose from 'mongoose'
+
+// Connection URL
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
-import mongoose from 'mongoose'
-mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
-mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${mongoUri}`)
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
 })
--- template.js
+++ template.js
@@ -6,9 +6,9 @@ export default () => {
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <meta charset="utf-8">
         <title>MERN Skeleton</title>
-    </head>
+    </head> 
     <body>
-        <div id="root">Hello World</div>
+        <div id="root"></div>
         <script type="text/javascript" src="/dist/bundle.js"></script>
     </body>
     </html>`
