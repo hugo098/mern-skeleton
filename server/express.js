a3f84180ef79461efea258d156efd62510e4ce12
Hugo Rodriguez <hi.rodriguez1998@gmail.com>
2021-03-25 22:37:46 -0300

    Added server side routing support

--- server/express.js
+++ server/express.js
@@ -46,9 +46,31 @@ app.use('/', userRoutes)
 
 app.use('/', authRoutes)
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
     res.status(200).send(Template())
-})
+})*/
+
+app.get('*', (req, res) => {
+    const sheets = new ServerStyleSheets()
+    const context = {}
+    const markup = ReactDOMServer.renderToString(
+      sheets.collect(
+            <StaticRouter location={req.url} context={context}>
+              <ThemeProvider theme={theme}>
+                <MainRouter />
+              </ThemeProvider>
+            </StaticRouter>
+          )
+      )
+      if (context.url) {
+        return res.redirect(303, context.url)
+      }
+      const css = sheets.toString()
+      res.status(200).send(Template({
+        markup: markup,
+        css: css
+      }))
+  })
 
 
 // Catch unauthorised errors
